9e095a3a90ffb5aa0eee884f9b86b8e50925bc7a
Michael Erdmann <michael.erdmann@snafu.de>
2017-08-21 17:56:53 +0200

    [Prerelease] Bumped version number

--- gulpfile.js
+++ gulpfile.js
@@ -20,54 +20,54 @@ gulp.task("github-release", function(done) {
   },  done);
 });
 
-gulp.task('bump-version', function () {
+gulp.task("bump-version", function () {
 // We hardcode the version change type to 'patch' but it may be a good idea to
 // use minimist (https://www.npmjs.com/package/minimist) to determine with a
 // command argument whether you are doing a 'major', 'minor' or a 'patch' change.
-  return gulp.src(['./bower.json', './package.json'])
-    .pipe(bump({type: "patch"}).on('error', gutil.log))
-    .pipe(gulp.dest('./'));
+  return gulp.src(["./bower.json", "./package.json"])
+    .pipe(bump({type: "patch"}).on("error", gutil.log))
+    .pipe(gulp.dest("./"));
 });
 
-gulp.task('commit-changes', function () {
-  return gulp.src('.')
+gulp.task("commit-changes", function () {
+  return gulp.src(".")
     .pipe(git.add())
-    .pipe(git.commit('[Prerelease] Bumped version number'));
+    .pipe(git.commit("[Prerelease] Bumped version number"));
 });
 
-gulp.task('push-changes', function (cb) {
-  git.push('origin', 'master', cb);
+gulp.task("push-changes", function (cb) {
+  git.push("origin", "master", cb);
 });
 
-gulp.task('create-new-tag', function (cb) {
+gulp.task("create-new-tag", function (cb) {
   var version = getPackageJsonVersion();
-  git.tag(version, 'Created Tag for version: ' + version, function (error) {
+  git.tag(version, "Created Tag for version: " + version, function (error) {
     if (error) {
       return cb(error);
     }
-    git.push('origin', 'master', {args: '--tags'}, cb);
+    git.push("origin", "master", {args: "--tags"}, cb);
   });
 
-  function getPackageJsonVersion () {
+function getPackageJsonVersion () {
     // We parse the json file instead of using require because require caches
     // multiple calls so the version number won't be updated
-    return JSON.parse(fs.readFileSync('./package.json', 'utf8')).version;
+    return JSON.parse(fs.readFileSync("./package.json", "utf8")).version;
   };
 });
 
-gulp.task('release', function (callback) {
+gulp.task("release", function (callback) {
   runSequence(
-    'bump-version',
-    'changelog',
-    'commit-changes',
-    'push-changes',
-    'create-new-tag',
-    'github-release',
+    "bump-version",
+    "changelog",
+    "commit-changes",
+    "push-changes",
+    "create-new-tag",
+    "github-release",
     function (error) {
       if (error) {
         console.log(error.message);
       } else {
-        console.log('RELEASE FINISHED SUCCESSFULLY');
+        console.log("RELEASE FINISHED SUCCESSFULLY");
       }
       callback(error);
     });
